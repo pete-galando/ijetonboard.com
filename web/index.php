26fee11a38104ffeba4c109701a5e338175d3bc8
Sam Skjonsberg <skoneberg@gmail.com>
2013-01-31 22:09:04 -0800

    Home page content; Navigation and style tweaks.

--- web/index.php
+++ web/index.php
@@ -1,27 +1,35 @@
 <?php
     include_once $_SERVER['DOCUMENT_ROOT'] . '/init.php';
 
-    $title = $active_page_title = 'Home';
+    $title     = $active_page_title = 'Home';
     $base_href = BASE_URL;
 
     include_once WEB_ROOT . '/templates/head.php';
 
 ?>
-    <section id="banner"></section>
-    <section class="clearafter">
-        <div class="float-left column one-half">
-            <h2>Airlines</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fringilla orci sit amet metus gravida gravida. Donec gravida tincidunt augue at commodo. Donec rhoncus, massa eget condimentum euismod, neque dolor venenatis purus, sed porttitor odio odio at nulla.</p>
-            <p>Integer facilisis ipsum vitae felis lobortis id elementum quam.</p>
-            <a href="" class="button icon-plane">Airlines</a>
-        </div>
-        <div class="float-left column one-half">
-            <h2>Developers</h2>
-            <p>Mauris auctor, nisi interdum consequat eleifend, est enim cursus tellus, ac dapibus orci velit eget metus. Praesent tempus semper felis eu blandit. Quisque imperdiet, tortor in gravida dictum, orci velit adipiscing odio, tristique volutpat enim dui non lectus. Nam eget luctus purus.</p>
-            <p>Pellentesque at sollicitudin leo.</p>
-            <a href="" class="button icon-gear">Developers</a>
-        </div>
-    </section>
+<section id="banner"><img src="<?= BASE_URL ?>static/images/home-banner-1.jpg" width="960" height="300"
+                          alt="The US global airline industry is projected to grow to more than 40,000 aircraft by 2030">
+</section>
+<section class="clearafter">
+    <div class="float-left column one-half">
+        <h2>Airlines</h2>
+
+        <p>
+            Airline operators are coming to expect new capabilities from their airplanes. iJet provides an open platform
+            and application to connect the airlines with their fleet.
+        </p>
+        <a href="<?= BASE_URL; ?>airlines/" class="button icon-plane">Learn More</a>
+    </div>
+    <div class="float-left column one-half">
+        <h2>Developers</h2>
+
+        <p>
+            iJet allows developers to address the inefficiencies of the aviation market by rapidly developing and
+            deploying applications a fraction of the cost.
+        </p>
+        <a href="<?= BASE_URL ?>developers/" class="button icon-gear">Learn More</a>
+    </div>
+</section>
 <?php
 
     include_once WEB_ROOT . '/templates/foot.php';
--- web/templates/head.php
+++ web/templates/head.php
@@ -1,54 +1,51 @@
 <?php
 
-    if(!isset($base_href)) {
+    if (!isset($base_href)) {
         $base_href = $_SERVER['REQUEST_URL'];
     }
 
-    if(!isset($active_page_title)) {
+    if (!isset($active_page_title)) {
         $active_page_title = 'Home';
     }
 
-?><!DOCTYPE html><html>
-      <head>
-          <title>iJet ONBOARD&trade; | <?= htmlspecialchars($title); ?></title>
-          <base href="<?= $base_href; ?>">
-          <meta name="description" content="iJet Onboard is a platform and services company with a passion to transform commercial aviation. We are enabling the intelligent aircraft by building an open and secure platform which provides three foundational capabilities: systems management, data management and application hosting.">
-          <meta name="keywords" content="iJet Onboard,clean-tech, satellite, global satellite network, real-time, two-way, transportation industry, global satellite, coverage, aviation, connectivity, bandwidth, platform, black box streaming, weather information, voice communication, EFB enhancement, technology, software">
-          <? if(ENABLE_CUSTOM_FONT): ?><link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400,300,600,700" rel="stylesheet" type="text/css"><?php endif; ?>
-          <link href="/static/css/styles.css" rel="stylesheet" type="text/css">
-          <!--[if lt IE 9]><script src="/static/js/html5shiv.js"></script><![endif]-->
-      </head>
-      <body>
-          <header class="clearafter">
-              <a href="<?= BASE_URL; ?>"><img alt="iJet ONBOARD" src="/static/images/logo.gif" width="243" height="53" border="0"></a>
-              <nav>
-                  <ul class="clearafter">
-                    <?php
-                        foreach($pages as $title => $page_info) {
-                            $is_active = $title === $active_page_title || $title === $active_parent;
-                            echo '<li><a href="' . $page_info['url'] . '"' . ($is_active ? ' class="active"' : '') . '>' . $title . '</a>';
-                        }
-                    ?>
-                  </ul>
-                  <?php
-                    if(isset($pages[$active_parent]['children'])) {
-                        $is_developers  = $active_page_title === 'Developers' || $active_parent === 'Developers';
-                        $is_airlines    = $active_page_title === 'Airlines' || $active_parent === 'Airlines';
-                        $special_class  = '';
-                        if($is_developers) {
-                            $special_class = ' developers';
-                        } else if($is_airlines) {
-                            $special_class = ' airlines';
-                        }
-
-                        echo '<ul class="clearafter subnav' . $special_class . '">';
-                        foreach($pages[$active_parent]['children'] as $title => $page_info) {
-                            $is_active = $title === $active_page_title;
-                            echo '<li><a href="' . $page_info['url'] . '"' . ($is_active ? ' class="active"' : '') . '>' . $title . '</a>';
-                        }
-                        echo '</ul>';
-                    }
-
-                   ?>
-              </nav>
-          </header>
+?><!DOCTYPE html>
+<html>
+<head>
+    <title>iJet ONBOARD&trade; | <?= htmlspecialchars($title); ?></title>
+    <base href="<?= $base_href; ?>">
+    <meta name="description"
+          content="iJet Onboard is a platform and services company with a passion to transform commercial aviation. We are enabling the intelligent aircraft by building an open and secure platform which provides three foundational capabilities: systems management, data management and application hosting.">
+    <meta name="keywords"
+          content="iJet Onboard,clean-tech, satellite, global satellite network, real-time, two-way, transportation industry, global satellite, coverage, aviation, connectivity, bandwidth, platform, black box streaming, weather information, voice communication, EFB enhancement, technology, software">
+    <? if (ENABLE_CUSTOM_FONT): ?>
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400,300,600,700" rel="stylesheet"
+          type="text/css"><?php endif; ?>
+    <link href="/static/css/styles.css" rel="stylesheet" type="text/css">
+    <!--[if lt IE 9]>
+    <script src="/static/js/html5shiv.js"></script><![endif]-->
+</head>
+<body>
+<div id="rail"></div>
+<header class="clearafter">
+    <a href="<?= BASE_URL; ?>"><img alt="iJet ONBOARD" src="/static/images/logo.gif" width="292" height="92" border="0"></a>
+    <nav>
+        <ul class="clearafter">
+            <?php
+                foreach ($pages as $title => $page_info) {
+                    $is_active = $title === $active_page_title || $title === $active_parent;
+                    echo '<li><a href="' . $page_info['url'] . '"' . ($is_active ? ' class="active"' : '') . '>' . $title . '</a>';
+                }
+            ?>
+        </ul>
+        <?php
+            if (isset($pages[$active_parent]['children'])) {
+                echo '<div class="subnav"><ul class="clearafter">';
+                foreach ($pages[$active_parent]['children'] as $title => $page_info) {
+                    $is_active = $title === $active_page_title;
+                    echo '<li><a href="' . $page_info['url'] . '"' . ($is_active ? ' class="active"' : '') . '>' . $title . '</a>';
+                }
+                echo '</ul></div>';
+            }
+        ?>
+    </nav>
+</header>
