ec29511d776a16d4cb5666c43bd0f9ba8952c0f0
Pete Galando <pete.galando@ijetonboard.com>
2013-02-05 11:20:59 -0800

    About us overview text updates from Mike C

--- web/about/index.php
+++ web/about/index.php
@@ -11,9 +11,9 @@
     <section>
         <h1>About Us</h1>
         <p>
-        iJet's vision for the intelligent aircraft is to help our airline customers generate new sources of revenue and become more operationally efficient. We begin by changing the way applications are delivered to the aircraft. The iJet Platform will deliver a broad array of integrated solutions, services and data analytics to help our Customers achieve key tactical and strategic objectives and thrive amid environmental concerns and operational, economic and regulatory challenges.</p>
+        iJet's vision for the intelligent aircraft is to help our airline customers generate new sources of revenue, reduce costs, and improve operational efficiency. We begin by changing the way applications are delivered to the aircraft. iJet will deliver a broad array of integrated solutions, services and data analytics to help our Customers achieve key tactical and strategic objectives and thrive amid environmental concerns and operational, economic and regulatory challenges.</p>
         <p>
-Gone are the days of having to cost justify single point solutions. iJet's strategy is centered on a Platform as a Service delivery model.  We draw on previously proven design concepts from cloud-computing to evolve the aviation industry from the previous hardware-centric architecture to a shared software and services infrastructure.   Our Platform merges airline ground systems, communications links, and onboard systems into a single solution.  This approach enables a rich and continuous flow of data and information between the aircraft and the airline.
+Gone are the days of having to cost justify single point solutions. iJet's strategy is centered on a Platform-as-a-Service delivery model.  We draw on previously proven design concepts from cloud-computing to evolve the aviation industry from the previous hardware-centric architecture to a shared software and services infrastructure.   Our Platform merges airline ground systems, communications links, and onboard systems into a single solution.  This approach enables a rich and continuous flow of data and information between the aircraft and the airline.
 <br>
 We proudly deliver value from our first installation!
         </p>
